ad1b6c9c93e8006bf66f72519b277c6baff34d22
wtejada3 <wtejada3@gatech.edu>
2021-03-28 16:58:30 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+###################### In order to run the app enter: python -m flask run
+###################### has to be in: flask-resume-template folder
+
+
+import oyaml as yaml
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+
+@app.route('/')
+def index():
+    website_data = yaml.load(open('_config.yaml'))
+
+    return render_template('index.html', data=website_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+    #app.run()
