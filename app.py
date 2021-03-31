f3798f3667ae3b67d9da7646a0a62bcbd36c03ae
wtejada3 <wtejada3@gatech.edu>
2021-03-30 23:23:38 -0700

    Added Screenshot to Projects section

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 import oyaml as yaml
 from flask import Flask
 from flask import render_template
+import pandas as pd
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -18,7 +19,16 @@ def index():
 
 @app.route('/d3-examples')
 def graph():
-    return render_template('d3-graph/graph.html')
+    graph_data = pd.read_csv('board_games.csv')
+    return render_template('d3-graph/graph.html', data = graph_data)
+
+@app.route('/powerbi')
+def power_bi():
+    return render_template('power_bi.html')
+
+@app.route('/dashboard-screenshots')
+def dashboard_screenshots():
+    return render_template('dashboard-images.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
